e287795561ee24b0e376b56a8ab22680cf84a801
Leah Yukelson <leahyukelson@gmail.com>
2017-08-31 15:50:31 -0700

    Implement Google OAuth on the backend

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' });
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refresh token', refreshToken);
+			console.log('profile:', profile);
+		}
+	)
+);
 
-// Run-time configuration for port number (production) or 5000 (development)
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+// Port run-time configuration (production) or 5000 (development)
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
