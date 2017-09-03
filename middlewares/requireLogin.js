4ce37656b2982204ef6beb12aa5b39bb828fdf2d
Leah Yukelson <leahyukelson@gmail.com>
2017-09-02 17:04:08 -0700

    Show user credits in header

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,7 +1,9 @@
 // Middleware that checks that a user is logged in and continues to next middleware if so
 module.exports = (req, res, next) => {
-	if (!user) {
-		return res.status(401).send({ error: 'There must be a user logged in' });
+	if (!req.user) {
+		return res
+			.status(401)
+			.send({ error: 'There must be a user logged in' });
 	}
 
 	next();
